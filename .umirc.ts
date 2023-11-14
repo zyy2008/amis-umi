bb93b214abec03ff15991aca298dc6dcf0258b2e
zyy <zyy2008@vip.qq.com>
2023-11-14 17:12:56 +0800

    tijiao

--- .umirc.ts
+++ .umirc.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from '@umijs/max';
+import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin';
 
 export default defineConfig({
   antd: {},
@@ -6,6 +7,14 @@ export default defineConfig({
   model: {},
   initialState: {},
   request: {},
+  chainWebpack: (memo) => {
+    // 更多配置 https://github.com/Microsoft/monaco-editor-webpack-plugin#options
+    memo.plugin('monaco-editor-webpack-plugin').use(MonacoWebpackPlugin, [
+      // 按需配置
+      { languages: ['javascript'] },
+    ]);
+    return memo;
+  },
   layout: {
     title: '@umijs/max',
   },
